08447f927c142cb569a02f4a2bbf0effc30141e7
nico_martin <mail@nicomartin.ch>
2020-03-03 07:44:32 +0100

    added auth for push endpoint

--- app/routes/createPush.ts
+++ app/routes/createPush.ts
@@ -6,6 +6,22 @@ export const createPush = async (
   req: express.Request,
   res: express.Response
 ) => {
+  // Auth
+  const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+  const [login, password] = Buffer.from(b64auth, "base64")
+    .toString()
+    .split(":");
+  const appPassword = String(process.env.AUTH_ADMIN_PASSWORD);
+
+  if (
+    appPassword !== "undefined" &&
+    (login !== "admin" || password !== appPassword)
+  ) {
+    res.status(401).send({
+      error: "Authentication failed"
+    });
+  }
+
   const title = req.body.title;
   const body = req.body.body;
 
